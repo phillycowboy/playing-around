b15799d2dd4132cac942debc34e8cc3e5a81cc14
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-12-11 15:24:04 -0500

    added logic

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 console.log("%cconnented", "color: red");
+
+let btn = document.querySelector("#btn");
+let message = document.querySelector("#message");
+message.style.display = "none";
+
+btn.addEventListener("click", (e) => {
+    console.log(e);
+    if(message.style.display === "none"){
+        message.style.display = "block"
+    }else{
+        message.style.display = "none";
+    }
+})
+
+// function hideButton() {
+//     if(message.style.display === "block"){
+//         message.style.display = "none"
+//     }
+// }
